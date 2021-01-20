ff1bc0c1fb3af113725149d61aa172bc0673130c
Saleh <cobecode7@gmail.com>
2021-01-20 03:52:23 +0300

    add static and media

--- post/models.py
+++ post/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=1000)
-    created_at = models.DateField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
     image = models.ImageField(upload_to='post-img/')
 
     class Meta:
@@ -13,7 +13,7 @@ class Post(models.Model):
         verbose_name_plural = ("Posts")
 
     def __str__(self):
-        return self.name
+        return self.title
 
     def get_absolute_url(self):
         return reverse("Post_detail", kwargs={"pk": self.pk})
