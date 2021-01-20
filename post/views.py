9c52646e8bc8c2211c1b0df63ba30fefd3345259
Saleh <cobecode7@gmail.com>
2021-01-21 02:29:34 +0300

    add all parts the project its end

--- post/views.py
+++ post/views.py
@@ -5,8 +5,19 @@ from django.views.generic import ListView , DetailView ,CreateView ,UpdateView ,
 
 
 class PostList(ListView):
-    model = Post
-
+    model = Post               ## in template [ object_list , post_list ]
+    #context_object_name = 'all_post'
+    ordering =['-created_at']
+    #queryset = Post.objects.filter(active=True)
+    #template_name = 'post/test.html'
+    def get_queryset(self):
+        return Post.objects.filter(active=True)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["myname"] = 'Saleh Mohamad'
+        return context
+    
 
 class PostDetail(DetailView):
     model = Post
